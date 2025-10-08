5fb4fbd7f58757c1c76701e61b45a65de10c3925
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-10-08 18:15:59 +0300

    neural noise algo

--- gradient.glsl
+++ gradient.glsl
@@ -34,12 +34,35 @@ float fbm(vec2 p)
   return value;
 }
 
+float neuralNoise(vec2 p, float time)
+{
+  vec2 n = p * 0.0;
+  float scale = 3.0;
+  float accum = 0.0;
+
+  float c = 0.74;
+  float s = 0.7;
+  mat2 m = mat2(c, -s, s, c);
+
+  for (float i = 0.0; i < 15.0; i++)
+    {
+      p *= m;
+      n *= m;
+
+      vec2 q = p * scale  + n - time;
+      accum += dot(cos(q), vec2(1.8)) / scale;
+      n += sin(q);
+      scale *= 1.2;
+    }
+
+  return accum;
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-  vec2 uv = (fragCoord + fragCoord - iResolution.xy);
-  uv = uv / iResolution.y;
+  vec2 uv = (fragCoord + fragCoord - iResolution.xy) / iResolution.y;
 
-  float time = iTime * 0.3;
+  float time = iGlobalTime * 0.3;
   float timeCos = cos(time);
   float timeSin = sin(time);
   mat2 rotate = mat2(timeCos, -timeSin, timeSin, timeCos);
@@ -52,15 +75,18 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
   vec2 noiseCoord = uv * 2.0 + time * 0.5;
   float n = fbm(noiseCoord);
 
-  float pattern = dist * 3.0 + n * 2.0 - time;
+  float neural = neuralNoise(uv * 1.5, time * 2.0);
+
+  float pattern = dist * 3.0 + n * 2.0 + neural * 0.5 - time;
   
   vec3 col = vec3(
-		  0.5 + 0.5 * sin(pattern),
+		  0.5 + 0.5 * sin(pattern + neural),
 		  0.5 + 0.5 * cos(angle * 2.0 + n * 2.0 + time),
-		  0.5 + 0.5 * sin(pattern * 1.3 + n)
+		  0.5 + 0.5 * sin(pattern * 1.3 + neural * 0.8)
 		  );
 
   col *= 1.0 - dist * 0.3;
+  col *= 0.9 + 0.1 * neural;
   
   
   fragColor = vec4(col, 1.0);
